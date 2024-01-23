76667375f188bb6fc9235df8c858fc1b83cd3911
Sergey <Sr233@yandex.ru>
2024-01-23 22:36:40 +0300

    implemented list, delete, read

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import fs from "fs/promises";
+
+const pathToFiles = process.cwd() + "\\src\\fs\\";
+
+async function dirOrFileExist(path) {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
 const remove = async () => {
-    // Write your code here 
+  const existFileForDelete = await dirOrFileExist(
+    pathToFiles + "files\\fileToRemove.txt"
+  );
+
+  if (existFileForDelete) {
+    await fs.rm(pathToFiles + "files\\fileToRemove.txt");
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,28 @@
+import fs from "fs/promises";
+
+const pathToFiles = process.cwd() + "\\src\\fs\\";
+
+async function dirOrFileExist(path) {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
 const list = async () => {
-    // Write your code here 
+  const existFilesFolder = await dirOrFileExist(pathToFiles + "files");
+
+  if (existFilesFolder) {
+    const data = await fs.readdir(pathToFiles + "files");
+
+    for (const name of data) {
+      process.stdout.write(name + "\n");
+    }
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
