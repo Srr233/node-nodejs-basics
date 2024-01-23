b7fc794557c0337359be5ded393bb587987622f5
Sergey <Sr233@yandex.ru>
2024-01-23 21:27:29 +0300

    implemented create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises";
+
+const pathToFiles = process.cwd() + "\\src\\fs\\files";
 const create = async () => {
-    // Write your code here 
+  await fs
+    .access(pathToFiles + "\\fresh.txt")
+    .then(() => {
+      throw Error("FS operation failed");
+    })
+    .catch(async (err) => {
+      if (err.message === "FS operation failed") throw err;
+      await fs.writeFile(pathToFiles + "\\fresh.txt", "I am fresh and young");
+    });
 };
 
 await create();
