726b2e1ab390972bf91007884fe6b38f23882099
Sergey <Sr233@yandex.ru>
2024-01-25 12:26:52 +0300

    implement compress, decompress func

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import zlib from "zlib";
+import fs from "fs";
+
+const pathToFile = process.cwd() + "\\src\\zip\\files\\";
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(pathToFile + "fileToCompress.txt");
+  const writeStream = fs.createWriteStream(pathToFile + "archive.gz");
+
+  readStream.pipe(zlib.createGzip()).pipe(writeStream);
 };
 
 await compress();
